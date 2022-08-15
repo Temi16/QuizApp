ba345862f295a826ec9243a7b42fa808c78c18a2
Rauf Roqeeb Temidayo <raufroqeeb123@gmail.com>
2022-08-16 00:23:19 +0100

    Created AdminInterface

---
+++ QuizApp/Interface/IRepository/IAdminRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using QuizApp.DTO;
+
+namespace QuizApp.Interface
+{
+    public interface IAdminRepository
+    {
+        public Task<AdminDTO> CraeteAdmin();
+        public Task<AdminDTO> UpdateAdmin();
+        public Task<AdminDTO> DeleteAdmin();
+        public Task<IList<AdminDTO>> GetAllAdmin();
+        public Task<AdminDTO> GetAdminById(int id);
+
+
+    }
+}
