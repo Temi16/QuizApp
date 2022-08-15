034faa762485234043ccdd500a8d1db4e5ae12a0
MahmudMartinet <mkoadeleke9806@gmail.com>
2022-08-15 15:57:20 -0700

    Adding Context

---
+++ QuizApp/Contexts/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace QuizApp.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Learner> Learners { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Option> Options { get; set; }
+
+    }
+}
+
