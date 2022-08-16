ab662a15703290b23b36a1a1b92cc49bec379765
MahmudMartinet <mkoadeleke9806@gmail.com>
2022-08-16 10:38:12 -0700

    Adding user repository

---
+++ QuizApp/Interface/IRepository/ILearnerRepository.cs
@@ -0,0 +1,13 @@
+﻿using QuizApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface ILearnerRepository
+{
+    Task<bool> CreateLearnerAsync(Learner learner);
+    Task<bool> EditLearnerAsync(Learner learner);
+    Task<Learner> GetLearnerAsync(int id);
+    Task<ICollection<Learner>> GetLearnersAsync();
+    
+}
