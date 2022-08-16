a52630e48e2a0d5f92e959d3041241973234378f
MahmudMartinet <mkoadeleke9806@gmail.com>
2022-08-16 11:51:40 -0700

    Adding request

---
+++ QuizApp/DTO/RequestModel/CreateAdminRequest.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace QuizApp.DTO.RequestModel
+{
+    public class CreateAdminRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
---
+++ QuizApp/DTO/RequestModel/CreateQuestionRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizApp.DTO.RequestModel
+{
+    public class CreateQuestionRequestModel
+    {
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+        public string Answer { get; set; }
+
+    }
+}
